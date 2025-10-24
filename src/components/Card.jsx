c336f2ada30200914194dc3584d258cea403ed30
박한별 <seohyun062807@naver.com>
2025-10-24 14:03:26 +0900

    완료

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,12 +7,12 @@ export default function Card({ item }) {
     return (
         <div className="card">
             <div className="card-image">
-                <img src={`https://placehold.co/600x400`} />
+                <img src={`https://placehold.co/600x400`} alt={item.titie} />
             </div>
             <div className="card-content">
-                <span className="card-category">???</span>
-                <h2 className="card-title">???</h2>
-                <p className="card-description">???</p>
+                <span className="card-category">{item.category}</span>
+                <h2 className="card-title">{item.title}</h2>
+                <p className="card-description">{item.description}</p>
             </div>
         </div>
     );
